3f001eaedfd34d0d83a9963f24214ea770ed3760
Bill Cathey <billcathey@hotmail.com>
2023-12-15 22:07:06 -0500

    Fetch data, create and style table of characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState({data: []})
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:5000/words'
+      )
+      setData(result.data)
+      console.log(result.data)
+    }
+    fetchData()
+  }, [data])
 
   return (
     <>
       <div className='App'>
-        <h1>Hello World!</h1>
+        <h1>Characters</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Mandarin</th>
+              <th>Pinyin</th>
+              <th>English</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.data.map(w => (
+              <tr key={w.id}>
+                <td>{w.mandarin}</td>
+                <td>{w.pinyin}</td>
+                <td>{w.english}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   )
