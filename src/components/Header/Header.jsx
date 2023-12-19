29c7a815bc8ae49a6c7de9171631e9c419722ef9
Bill Cathey <billcathey@hotmail.com>
2023-12-18 20:13:37 -0500

    Update Header component links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import './Header.css'
 
 const Header = () => (
     <nav className="nav">
         <div className="logo">
-            <Link to="/home" className="nav-link">Mandarin</Link>
+            <Link to="/" className="nav-link">Mandarin</Link>
         </div>
         <div className="nav-content">
-            <Link to="/characters" className="nav-link">Characters</Link>
-            <Link to="/study" className="nav-link">Study</Link>
-            <Link to="/about" className="nav-link">About</Link>
+            <NavLink to="/home" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>List</NavLink>
+            <NavLink to="/cards" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>Cards</NavLink>
+            <NavLink to="/about" className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>About</NavLink>
         </div>
     </nav>
 )
