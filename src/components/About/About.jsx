147b23bce1f15eb56fac79ee93affb3a1df6bf2d
Bill Cathey <billcathey@hotmail.com>
2024-02-07 16:11:36 -0500

    Edit About page text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import './About.css'
 const About = () => (
     <div className="about">
         <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta debitis quas obcaecati ducimus recusandae sit? Cumque ducimus perspiciatis reprehenderit quos, quasi incidunt eum in? Rem impedit ratione delectus alias ipsa laudantium nulla non iusto exercitationem? Eos nesciunt, voluptas tempore rem rerum nobis soluta! Corrupti qui ullam obcaecati modi doloremque hic ad, commodi tenetur iusto, eum repellendus. Quidem sunt voluptatibus, aperiam quaerat perferendis pariatur qui ipsum fuga, est totam, reiciendis numquam assumenda deserunt deleniti dolorum veniam labore. Non perferendis eum nisi, quasi enim veritatis aliquid sequi at alias beatae, tempora animi unde eaque voluptatem natus distinctio, et omnis id tenetur ea!</p>
+        <p>Mandarin Characters is an application for learning and practicing Mandarin vocabulary. It includes a listing of the most frequently used words in the Chinese language, along with their pinyin transcriptions and English translations. Vocabulary cards provide a useful way to test your memory of what you have learned.</p>
     </div>
 )
 
